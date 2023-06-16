afc827b2f5e77c76134ef3634926c84c3cfacf2a
aigul <ms.aigulam@gmail.com>
2023-06-17 00:13:35 +0500

    page preloader

---
+++ src/Components/Common/Preloader/Preloader.js
@@ -0,0 +1,7 @@
+ import React from "react";
+ import spinner from "../../../assets/images/spinner.svg"
+
+let Preloader = (props) => {
+    return < img  src={spinner}/>
+}
+export default Preloader;
